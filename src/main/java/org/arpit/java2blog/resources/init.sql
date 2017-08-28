0191a657049b168a986f46caddc125fcebd28cc6
vvviiit <vvviiit@outlook.com>
2017-08-28 11:36:13 +0300

    sql script

--- src/main/java/org/arpit/java2blog/resources/init.sql
+++ src/main/java/org/arpit/java2blog/resources/init.sql
@@ -1,52 +1,43 @@
 CREATE TABLE IF NOT EXISTS user
 (
-  id INT(8) AUTO_INCREMENT NOT NULL PRIMARY KEY ,
-  name VARCHAR(25),
-  age INT(11),
-  adress VARCHAR(60),
-  isAdmin BIT(1),
+  id          INT(8) AUTO_INCREMENT NOT NULL PRIMARY KEY,
+  name        VARCHAR(25),
+  age         INT(11),
+  adress      VARCHAR(60),
+  isAdmin     BIT(1),
   createdDate TIMESTAMP
 );
 
-INSERT INTO user (name, age, isAdmin) VALUES ('Vasya', 27, 1);
-INSERT INTO user (name, age, isAdmin) VALUES ('Petya', 24, 0);
-INSERT INTO user (name, age, isAdmin) VALUES ('Sasha', 20, 1);
-INSERT INTO user (name, age, isAdmin) VALUES ('Vasya', 27, 1);
-INSERT INTO user (name, age, isAdmin) VALUES ('Petya', 24, 0);
-INSERT INTO user (name, age, isAdmin) VALUES ('Sasha', 20, 1);
-INSERT INTO user (name, age, isAdmin) VALUES ('Vasya', 27, 1);
-INSERT INTO user (name, age, isAdmin) VALUES ('Petya', 24, 0);
-INSERT INTO user (name, age, isAdmin) VALUES ('Sasha', 20, 1);
-INSERT INTO user (name, age, isAdmin) VALUES ('Vasya', 27, 1);
-INSERT INTO user (name, age, isAdmin) VALUES ('Petya', 24, 0);
-INSERT INTO user (name, age, isAdmin) VALUES ('Sasha', 20, 1);
-INSERT INTO user (name, age, isAdmin) VALUES ('Vasya', 27, 1);
-INSERT INTO user (name, age, isAdmin) VALUES ('Petya', 24, 0);
-INSERT INTO user (name, age, isAdmin) VALUES ('Sasha', 20, 1);
-INSERT INTO user (name, age, isAdmin) VALUES ('Vasya', 27, 1);
-INSERT INTO user (name, age, isAdmin) VALUES ('Petya', 24, 0);
-INSERT INTO user (name, age, isAdmin) VALUES ('Sasha', 20, 1);
-INSERT INTO user (name, age, isAdmin) VALUES ('Vasya', 27, 1);
-INSERT INTO user (name, age, isAdmin) VALUES ('Petya', 24, 0);
-INSERT INTO user (name, age, isAdmin) VALUES ('Sasha', 20, 1);
-INSERT INTO user (name, age, isAdmin) VALUES ('Vasya', 27, 1);
-INSERT INTO user (name, age, isAdmin) VALUES ('Petya', 24, 0);
-INSERT INTO user (name, age, isAdmin) VALUES ('Sasha', 20, 1);
-INSERT INTO user (name, age, isAdmin) VALUES ('Vasya', 27, 1);
-INSERT INTO user (name, age, isAdmin) VALUES ('Petya', 24, 0);
-INSERT INTO user (name, age, isAdmin) VALUES ('Sasha', 20, 1);
-INSERT INTO user (name, age, isAdmin) VALUES ('Vasya', 27, 1);
-INSERT INTO user (name, age, isAdmin) VALUES ('Petya', 24, 0);
-INSERT INTO user (name, age, isAdmin) VALUES ('Sasha', 20, 1);
-INSERT INTO user (name, age, isAdmin) VALUES ('Vasya', 27, 1);
-INSERT INTO user (name, age, isAdmin) VALUES ('Petya', 24, 0);
-INSERT INTO user (name, age, isAdmin) VALUES ('Sasha', 20, 1);
-INSERT INTO user (name, age, isAdmin) VALUES ('Vasya', 27, 1);
-INSERT INTO user (name, age, isAdmin) VALUES ('Petya', 24, 0);
-INSERT INTO user (name, age, isAdmin) VALUES ('Sasha', 20, 1);
-INSERT INTO user (name, age, isAdmin) VALUES ('Vasya', 27, 1);
-INSERT INTO user (name, age, isAdmin) VALUES ('Petya', 24, 0);
-INSERT INTO user (name, age, isAdmin) VALUES ('Sasha', 20, 1);
-INSERT INTO user (name, age, isAdmin) VALUES ('Vasya', 27, 1);
-INSERT INTO user (name, age, isAdmin) VALUES ('Petya', 24, 0);
-INSERT INTO user (name, age, isAdmin) VALUES ('Sasha', 20, 1);
+INSERT INTO user (name, age, adress, isAdmin) VALUES ('Vasya', 27, 'Moscow', 1);
+INSERT INTO user (name, age, adress, isAdmin) VALUES ('Petya', 24, 'Piter', 0);
+INSERT INTO user (name, age, adress, isAdmin) VALUES ('Sasha', 20, 'Voronezh', 1);
+INSERT INTO user (name, age, adress, isAdmin) VALUES ('Vasya', 27, 'Moscow', 1);
+INSERT INTO user (name, age, adress, isAdmin) VALUES ('Petya', 24, 'Piter', 0);
+INSERT INTO user (name, age, adress, isAdmin) VALUES ('Sasha', 20, 'Voronezh', 1);
+INSERT INTO user (name, age, adress, isAdmin) VALUES ('Vasya', 27, 'Moscow', 1);
+INSERT INTO user (name, age, adress, isAdmin) VALUES ('Petya', 24, 'Piter', 0);
+INSERT INTO user (name, age, adress, isAdmin) VALUES ('Sasha', 20, 'Voronezh', 1);
+INSERT INTO user (name, age, adress, isAdmin) VALUES ('Vasya', 27, 'Moscow', 1);
+INSERT INTO user (name, age, adress, isAdmin) VALUES ('Petya', 24, 'Piter', 0);
+INSERT INTO user (name, age, adress, isAdmin) VALUES ('Sasha', 20, 'Voronezh', 1);
+INSERT INTO user (name, age, adress, isAdmin) VALUES ('Vasya', 27, 'Moscow', 1);
+INSERT INTO user (name, age, adress, isAdmin) VALUES ('Petya', 24, 'Piter', 0);
+INSERT INTO user (name, age, adress, isAdmin) VALUES ('Sasha', 20, 'Voronezh', 1);
+INSERT INTO user (name, age, adress, isAdmin) VALUES ('Vasya', 27, 'Moscow', 1);
+INSERT INTO user (name, age, adress, isAdmin) VALUES ('Petya', 24, 'Piter', 0);
+INSERT INTO user (name, age, adress, isAdmin) VALUES ('Sasha', 20, 'Voronezh', 1);
+INSERT INTO user (name, age, adress, isAdmin) VALUES ('Vasya', 27, 'Moscow', 1);
+INSERT INTO user (name, age, adress, isAdmin) VALUES ('Petya', 24, 'Piter', 0);
+INSERT INTO user (name, age, adress, isAdmin) VALUES ('Sasha', 20, 'Voronezh', 1);
+INSERT INTO user (name, age, adress, isAdmin) VALUES ('Vasya', 27, 'Moscow', 1);
+INSERT INTO user (name, age, adress, isAdmin) VALUES ('Petya', 24, 'Piter', 0);
+INSERT INTO user (name, age, adress, isAdmin) VALUES ('Sasha', 20, 'Voronezh', 1);
+INSERT INTO user (name, age, adress, isAdmin) VALUES ('Vasya', 27, 'Moscow', 1);
+INSERT INTO user (name, age, adress, isAdmin) VALUES ('Petya', 24, 'Piter', 0);
+INSERT INTO user (name, age, adress, isAdmin) VALUES ('Sasha', 20, 'Voronezh', 1);
+INSERT INTO user (name, age, adress, isAdmin) VALUES ('Vasya', 27, 'Moscow', 1);
+INSERT INTO user (name, age, adress, isAdmin) VALUES ('Petya', 24, 'Piter', 0);
+INSERT INTO user (name, age, adress, isAdmin) VALUES ('Sasha', 20, 'Voronezh', 1);
+INSERT INTO user (name, age, adress, isAdmin) VALUES ('Vasya', 27, 'Moscow', 1);
+INSERT INTO user (name, age, adress, isAdmin) VALUES ('Petya', 24, 'Piter', 0);
+INSERT INTO user (name, age, adress, isAdmin) VALUES ('Sasha', 20, 'Voronezh', 1);
